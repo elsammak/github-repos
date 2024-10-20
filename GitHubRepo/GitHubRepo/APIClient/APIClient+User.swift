6936d731de6eb31b674b5d8f33aa1527882f6c40
Mohammed Elsammak <elsamak_1@yahoo.com>
2024-10-20 11:37:18 +0300

    Remove hardcoded token and use environment variable

---
+++ GitHubRepo/GitHubRepo/APIClient/APIClient+User.swift
@@ -0,0 +1,92 @@
+//
+//  APIClient+User.swift
+//  GitHubRepo
+//
+//  Created by Mohammed Elsammak on 2024/10/20.
+//
+
+import Foundation
+extension APIClient {
+
+    /**
+     Fetch list of users. If nextUserUrl is nil, it starts with the first page of users.
+     With every response the next request url is included inside the header and should be used for the next request.
+     - Returns: UserJson object and next request URL if succeeded, or throws an error if failed.
+     */
+    func getUsers(fromUrl nextUserUrl: String? = nil) async -> (Result<([UserJSON], String?), ChatError>) {
+            // Create url request
+            var urlString: String!
+            if let urlStoredValue = nextUserUrl {
+                urlString = urlStoredValue
+            } else {
+                urlString = baseURL + "/users?"
+            }
+
+            guard let url = URL(string: urlString) else {
+                var chatError = ChatError()
+                chatError.errorMessage = "INVALID_URL"
+                return .failure(chatError)
+            }
+
+            var request = URLRequest(url: url)
+            
+
+            if let githubToken = ProcessInfo.processInfo.environment["ACCESS_TOKEN"] {
+                request.setValue("token \(githubToken)", forHTTPHeaderField: "Authorization")
+            } else {
+                var chatError = ChatError()
+                chatError.errorMessage = "No GitHub token found"
+                return .failure(chatError)
+            }
+        
+            do {
+                let (data, response) = try await URLSession.shared.data(for: request)
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    var chatError = ChatError()
+                    chatError.errorMessage = "Invalid response"
+                    return .failure(chatError)
+                }
+                
+
+                let decoder = JSONDecoder()
+                let usersArray = try decoder.decode([UserJSON].self, from: data)
+
+                var nextUrl: String?
+                if let httpResponse = response as? HTTPURLResponse, let str = httpResponse.allHeaderFields["Link"] as? String {
+                    nextUrl = extractNextUrl(fromString: str)
+                }
+
+                return .success((usersArray, nextUrl))
+            } catch {
+                var chatError = ChatError()
+                chatError.errorMessage = "USERS_PARSING_ERROR"
+                return .failure(chatError)
+            }
+        }
+    
+
+    /**
+     Get next request URL from the response header.
+     
+     - parameter string: `Link` field in the response header.
+     
+     - Returns: Next request url string if available, else nil.
+     */
+    func extractNextUrl(fromString string: String) -> String? {
+        let array = string.components(separatedBy: ",")
+        for element in array {
+            let part = element.components(separatedBy: ";")
+            if part[1].trimmingCharacters(in: .whitespacesAndNewlines) == "rel=\"next\"" {
+                let nextUrl = part[0]
+                if let start = nextUrl.firstIndex(of: "<"), let end = nextUrl.firstIndex(of: ">") {
+                    let startIndex = nextUrl.index(after: start) // Move the index after "<"
+                    let endIndex = nextUrl.index(before: end) // Move the index before ">"
+                    return String(nextUrl[startIndex..<endIndex]) // Return the substring between "<" and ">"
+                }
+
+            }
+        }
+        return nil
+    }
+}

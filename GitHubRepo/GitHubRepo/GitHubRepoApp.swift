1a551d8119d4e02c292f651c76e21748cf1705ad
Mohammed Elsammak <elsamak_1@yahoo.com>
2024-10-20 07:05:31 +0300

    Add New Project

---
+++ GitHubRepo/GitHubRepo/GitHubRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepoApp.swift
+//  GitHubRepo
+//
+//  Created by Mohammed Elsammak on 2024/10/20.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
